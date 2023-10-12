de2de3bb94474f4b8f7d2657753e04cc4d447950
hudaismail84@yahoo.com <hudaismail84@yahoo.com>
2023-10-12 18:54:06 +0300

    _

---
+++ main.js
@@ -0,0 +1,11 @@
+let img = document.querySelector('.img');
+let cont1 = document.querySelector('.cont1');
+
+
+function shirt(shirt){
+	img.src = shirt;
+}
+
+function colors(color){
+		cont1.style.background = color;
+}
